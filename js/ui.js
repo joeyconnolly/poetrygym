29cb2715a8a67ab5fcb986b7cb9d1bb2ad43bbf0
joeyconnolly <joeyrconnolly@gmail.com>
2025-02-26 18:16:15 +0000

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -299,3 +299,49 @@ const UI = (function() {
         showError
     };
 })();
+
+// Add these new functions to the UI module
+
+/**
+ * Show loading state
+ */
+function showLoadingState() {
+    // Make sure the loading indicator exists
+    if (!elements.loadingIndicator) {
+        elements.loadingIndicator = document.createElement('div');
+        elements.loadingIndicator.className = 'loading-indicator';
+        elements.loadingIndicator.innerHTML = '<span>Loading exercises...</span>';
+        elements.exerciseCard.appendChild(elements.loadingIndicator);
+    } else {
+        elements.loadingIndicator.classList.remove('hidden');
+    }
+    
+    // Disable generate button during loading
+    if (elements.generateButton) {
+        elements.generateButton.disabled = true;
+        elements.generateButton.classList.add('disabled');
+    }
+}
+
+/**
+ * Hide loading state
+ */
+function hideLoadingState() {
+    // Hide the loading indicator
+    if (elements.loadingIndicator) {
+        elements.loadingIndicator.classList.add('hidden');
+    }
+    
+    // Enable generate button
+    if (elements.generateButton) {
+        elements.generateButton.disabled = false;
+        elements.generateButton.classList.remove('disabled');
+    }
+}
+
+// Add these functions to the public API return statement
+return {
+    // ... existing functions
+    showLoadingState,
+    hideLoadingState
+};
